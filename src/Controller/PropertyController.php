a0dd51f7a76ad632d33fe68dc4057bb29f91f2ca
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-03-09 20:16:10 +0100

    Create Entity + Repository + using ObjectManager

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class PropertyController extends AbstractController
 {
     /**
+     * @var PropertyRepository;
+     */
+    private $propertyRepository;
+
+    public function __construct(PropertyRepository $propertyRepository, ObjectManager $om) {
+        $this->propertyRepository = $propertyRepository;
+        $this->om = $om;
+    }
+
+    /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
-    public function index(): Response
+    public function index(PropertyRepository $propertyRepository): Response
     {
+
+        $property = $this->propertyRepository->findAllUnsoldedHome();
+        dump($property);
+        $this->om->flush();
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties' 
         ]);
